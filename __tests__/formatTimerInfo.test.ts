f8f2bb56bb06547e3127e7087408df7aa5600503
alicja.mruk <alicja.mruk@treelineinteractive.com>
2022-11-09 20:42:34 +0100

    cleanup

--- __tests__/formatTimerInfo.test.ts
+++ __tests__/formatTimerInfo.test.ts
@@ -33,23 +33,7 @@ describe("formatTimeToTimestamp test", () => {
     expect(result).toEqual(expected);
   });
 
-  it("should return '1 .. n minutes ago' text if time is > 60s'", () => {
-    const currentDate = new Date();
-    currentDate.setHours(20, 2, 0);
-    const lastSeenDate = new Date();
-    lastSeenDate.setHours(20, 0, 0);
-
-    const expected = "1 .. n minutes ago";
-
-    const result = formatTimerInfo(
-      lastSeenDate.getTime(),
-      currentDate.getTime()
-    );
-
-    expect(result).toEqual(expected);
-  });
-
-  it("should return '1 .. n minutes ago' text if time is < 60s'", () => {
+  it("should return '0 ... 60 seconds ago' text if time is < 60s'", () => {
     const currentDate = new Date();
     currentDate.setHours(20, 0, 10);
     const lastSeenDate = new Date();
